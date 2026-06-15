f18f51567d15e71198f8f86b6c1c8957d20012f1
Nabintakhadka <diyaloyd123@gmail.com>
2026-06-15 09:09:02 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+```javascript
+function shopNow() {
+    alert("Welcome to StyleHub Clothing!");
+}
+
+function sendMessage() {
+    alert("Thank you for contacting StyleHub Clothing!");
+    return false;
+}
+```
